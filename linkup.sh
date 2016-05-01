d580e8aa7f72cb885b5a976253440692a3a1e8c3
Oleh Stolyar <stolyar.oleh@gmail.com>
2016-05-01 17:14:41 +0300

    Add ideavimrc

--- linkup.sh
+++ linkup.sh
@@ -10,3 +10,5 @@ ln -Tsi "$ABS_PATH/zsh/zshrc" ~/.zshrc
 ln -Tsi "$ABS_PATH/git/gitconfig" ~/.gitconfig
 
 ln -Tsi "$ABS_PATH/spacemacs/spacemacs" ~/.spacemacs
+
+ln -Tsi "$ABS_PATH/intellij/ideavimrc" ~/.ideavimrc
