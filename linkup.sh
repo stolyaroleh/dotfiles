ca505e119134b65d8c3c313a4337845753af4bfd
Oleh Stolyar <stolyar.oleh@gmail.com>
2018-02-14 08:38:39 +0200

    Use stow to manage dotfiles

--- linkup.sh
+++ linkup.sh
@@ -1,14 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-ABS_PATH=`pwd`
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+pushd $SCRIPTPATH
 
-ln -Tsi "$ABS_PATH/zsh/prezto" ~/.zprezto
-ln -Tsi "$ABS_PATH/zsh/zpreztorc" ~/.zpreztorc
-ln -Tsi "$ABS_PATH/zsh/zshenv" ~/.zshenv
-ln -Tsi "$ABS_PATH/zsh/zshrc" ~/.zshrc
+stow git
+stow intellij
+stow spacemacs
+stow xmonad
+stow zsh
 
-ln -Tsi "$ABS_PATH/git/gitconfig" ~/.gitconfig
-
-ln -Tsi "$ABS_PATH/spacemacs/spacemacs" ~/.spacemacs
-
-ln -Tsi "$ABS_PATH/intellij/ideavimrc" ~/.ideavimrc
+popd
