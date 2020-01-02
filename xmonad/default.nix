01bd355935585be55c10fcf4724e90e8a81dffe9
Oleh Stolyar <stolyar.oleh@gmail.com>
2020-01-02 12:09:55 +0000

    Update XMonad recipe

--- xmonad/default.nix
+++ xmonad/default.nix
@@ -1,21 +1,7 @@
-{ compiler ? "ghc822"
-}:
 let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskell = pkgs.haskell // {
-        packages = pkgs.haskell.packages // {
-          "${compiler}" = pkgs.haskell.packages.${compiler}.override {
-            overrides = self: super: with self; {
-              xmonad = callPackage ./xmonad.nix {};
-              xmonad-contrib = callPackage ./xmonad-contrib.nix {};
-              xmonad-extras = callPackage ./xmonad-extras.nix {};
-            };
-          };
-        };
-      };
-    };
-  };
-  nixpkgs = import <nixpkgs> { inherit config; };
+  sources = import ../nixos/nix/sources.nix;
 in
-  nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./xmonad-config.nix {}
+  with import sources.stable {};
+  haskellPackages.developPackage {
+    root = ./.;
+  }
